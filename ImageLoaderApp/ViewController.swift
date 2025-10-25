eaf01d3fc2d4e84107213d41aa0cdf939f63dae9
Oleg Fedorov <olegajaro94@gmail.com>
2025-10-25 20:12:54 +0700

    Initial Commit

---
+++ ImageLoaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLoaderApp
+//
+//  Created by Олег Федоров on 25.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
